b8a93657ac2c2a940de86b851b79899b03826332
Matthew Jordan <matthewjordan@Matthews-MacBook-Air-98.local>
2025-12-12 15:24:09 -0500

    added NER

--- src/data.py
+++ src/data.py
@@ -13,8 +13,29 @@ nltk.download('vader_lexicon')
 from sklearn.feature_extraction import DictVectorizer
 nltk.download('averaged_perceptron_tagger_eng')
 from scipy.sparse import hstack
+import spacy
+nlp = spacy.load("en_core_web_sm")
 
 df = pd.read_csv("../data/NewsCategorizer.csv")
+
+def sub_leaves(tree, label):
+    return [
+        subtree.leaves()
+        for subtree in tree.subtrees()
+        if isinstance(subtree, nltk.Tree) and subtree.label() == label
+    ]
+
+def extract_ner_counts(tree, target_labels):
+    counts = {f"NER_{lbl}": 0 for lbl in target_labels}
+
+    for subtree in tree.subtrees():
+        if isinstance(subtree, nltk.Tree):
+            lbl = subtree.label()
+            if lbl in target_labels:
+                counts[f"NER_{lbl}"] += 1
+
+    return counts
+
 
 # filtering/cleaning data 
 df = df[["headline", "category"]]
@@ -49,8 +70,18 @@ for words in df_filt["tokenized_headline"]:
         #trigram = (words[i], words[i+1], words[i+2])
         #if trigram in trigram_counts:
             #trigram_counts[trigram] += 1
-       # else:
+        #else:
             #trigram_counts[trigram] = 1
+
+# NER feature extraction
+entity_labels = ["PERSON", "ORG", "GPE", "MONEY", "LOC", "FAC", "NORP"]
+ner_data = []
+for doc in nlp.pipe(df_filt["headline"], batch_size=1000):
+    counts = {"NER_PERSON": False, "NER_ORG": False, "NER_GPE": False, "NER_MONEY": False, "NER_LOC": False, "NER_FAC": False, "NER_NORP": False}
+    for ent in doc.ents:
+        if ent.label_ in ("PERSON", "ORG", "GPE", "MONEY", "LOC", "FAC", "NORP"):
+            counts[f"NER_{ent.label_}"] = True
+    ner_data.append(counts) 
 
 # Create bigram_counts per headline (Sparse matrix - (row, col), num of bigrams count)
 texts = [" ".join(word) for word in df_filt["tokenized_headline"]]
@@ -60,6 +91,7 @@ bigrams_data = vectorizer.fit_transform(texts)
 #trigram counts
 #vectorizer_tri = CountVectorizer(ngram_range=(3,3), max_features=5000)
 #trigrams_data = vectorizer_tri.fit_transform(texts)
+
 
 # unigram counts
 # Unigrams (single words)
@@ -96,6 +128,9 @@ for word_lst in df_filt["tokenized_headline"]:
 vec = DictVectorizer()
 pos_data = vec.fit_transform(pos_data)
 
+#ner features
+ner_matrix = vec.fit_transform(ner_data)
+
 # assign a sentiment number for each headline (we can likely use the compound number)
 sentiments = []
 
@@ -107,12 +142,15 @@ for headline in df_filt["tokenized_headline"]:
 # Final data/features: ngrams_data, word2vec_data, pos_data, sentiments
 unigrams_dense = unigrams_data.toarray()
 bigrams_dense = bigrams_data.toarray()
-#trigrams_dense = trigrams_data.toarray()
 pos_dense = pos_data.toarray()
 sentiment_dense = np.array([s['compound'] for s in sentiments]).reshape(-1, 1)
+ner_dense = ner_matrix.toarray()
+#trigrams_dense = trigrams_data.toarray()
 
 # Stack all features horizontally
-X = np.hstack([unigrams_dense, bigrams_dense, word2vec_data, pos_dense, sentiment_dense, headline_length_feature])
+X = np.hstack([unigrams_dense, bigrams_dense, word2vec_data, pos_dense, sentiment_dense, ner_dense,
+               #trigrams_dense
+              ])
 
 # Target labels
 y = df_filt['category'].values
